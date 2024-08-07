3d0c4468cf48eafe027290a2ca7bea10eda75d51
Bongeka28 <bongekamdluli17@gmail.com>
2024-08-07 14:10:46 +0200

    heading edited

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import OrganizerAccountDesktop from "./components/OrganizerAccountDesktop"
+// import AttendeeDashboard from './components/attendee/AttendeeDashboard'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
