2f78e640c4977d68e9c60dd38f96ebeaeec86a22
PhindileT <phindile.simelane250@gmail.com>
2024-08-07 10:56:14 +0200

    Organiser desktop

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import OrganizerAccountDesktop from "./components/OrganizerAccountDesktop"
-import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <OrganizerAccountDesktop/>
+    <OrganizerAccountDesktop />
   </React.StrictMode>
 )
