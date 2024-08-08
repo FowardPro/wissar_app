8f68dfdfb08275c8ba630cf6005ec2f69270acbf
Bongeka28 <bongekamdluli17@gmail.com>
2024-08-08 13:26:57 +0200

    Search events page

--- src/components/attendee/SearchEvents.js
+++ src/components/attendee/SearchEvents.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './SearchEvents.css'
+import eventpic from '../../assets/photos/sjava.png'
 import { FcSearch } from "react-icons/fc";
 function SearchEvents() {
   return (
@@ -9,6 +10,34 @@ function SearchEvents() {
         <div className="searchEvents-div">
         <FcSearch size={50}/>
         <input placeholder="Search the event you want to view"/>
+        </div>
+        <div className="searchevent-cards">
+            <h2>Suggeted Events</h2>
+            <div className="event-cards">
+                <h3>EVENT NAME</h3>
+                <div className="image-paragraps">
+                <img src={eventpic} alt="Event picture"></img>
+                <div className="search-paragraps">
+                <p>Location:</p>
+                <p>Start Date:</p>
+                <p>End Date:</p>
+                </div>
+                </div> 
+                
+            </div>
+            
+            <div className="event-cards">
+                <h3>EVENT NAME</h3>
+                <div className="image-paragraps">
+                <img src={eventpic} alt="Event picture"></img>
+                <div className="search-paragraps">
+                <p>Location:</p>
+                <p>Start Date:</p>
+                <p>End Date:</p>
+                </div>
+                </div> 
+                
+            </div>
         </div>
         <div className="searchButton-div">
             <button>SEARCH</button>
