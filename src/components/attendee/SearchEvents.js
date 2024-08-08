50efc9dc0a76de42af3ec5418eeb1583e27282b8
Bongeka28 <bongekamdluli17@gmail.com>
2024-08-08 14:26:10 +0200

    Editing the search events page

--- src/components/attendee/SearchEvents.js
+++ src/components/attendee/SearchEvents.js
@@ -11,29 +11,30 @@ function SearchEvents() {
         <FcSearch size={50}/>
         <input placeholder="Search the event you want to view"/>
         </div>
+        <h2>Suggested Events</h2>
         <div className="searchevent-cards">
-            <h2>Suggeted Events</h2>
+            
             <div className="event-cards">
-                <h3>EVENT NAME</h3>
+                <h3>TUT Fresher's Bash 2024</h3>
                 <div className="image-paragraps">
                 <img src={eventpic} alt="Event picture"></img>
                 <div className="search-paragraps">
-                <p>Location:</p>
-                <p>Start Date:</p>
-                <p>End Date:</p>
+                <p>TUT Fresher's(Pretoria. Arcadia. Arts)</p>
+                <p>Friday 10 May 2024, 16:00 PM-</p>
+                <p>Sat 11 May 2024, 06:00 AM</p>
                 </div>
                 </div> 
                 
             </div>
             
             <div className="event-cards">
-                <h3>EVENT NAME</h3>
+                <h3>Black & White Lifestyle boat Cruise</h3>
                 <div className="image-paragraps">
                 <img src={eventpic} alt="Event picture"></img>
                 <div className="search-paragraps">
-                <p>Location:</p>
-                <p>Start Date:</p>
-                <p>End Date:</p>
+                <p>Black & White Lifestyle boat Cruise:</p>
+                <p>Sat 17 August 2024, 15:00 PM</p>
+                <p>Sun 18 August 2024, 02:00 AM</p>
                 </div>
                 </div> 
                 
