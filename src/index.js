6adbedb4e0855cbd3405743ffc1bf9f958de0e9e
FowardPro <147938121+FowardPro@users.noreply.github.com>
2024-08-10 13:03:54 +0200

    testing dev branch

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
  import OrganizerAccountDesktop from "./components/OrganizerAccountDesktop"
  
-//  import AttendeeDashboard from './components/attendee/AttendeeDashboard'
+ import AttendeeDashboard from './components/attendee/AttendeeDashboard'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <OrganizerAccountDesktop />
+    <AttendeeDashboard />
   </React.StrictMode>
 )
