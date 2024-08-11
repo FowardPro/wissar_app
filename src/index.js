3675e0406d665dda18acc4c2e2b3d48bb9ef62b3
ezram <ezramagagane@gmail.com>
2024-08-11 17:33:00 +0200

    Was testing the artist page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import ReactDOM from "react-dom/client"
 import OrganizerAccountDesktop from "./components/OrganizerAccountDesktop"
 
 import AttendeeDashboard from './components/attendee/AttendeeDashboard'
-import ArtistHomePage from './components/ArtistHomePage'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <ArtistHomePage />
+    <OrganizerAccountDesktop />
   </React.StrictMode>
 )
