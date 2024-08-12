6f00a7c3d22ebbb749ebede7cdf7f909138d93c1
FowardPro <fowardnkuna6@gmail.com>
2024-08-12 10:23:33 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import OrganizerAccountDesktop from "./components/OrganizerAccountDesktop"
+import App from "./App"
 
 import AttendeeDashboard from './components/attendee/AttendeeDashboard'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <OrganizerAccountDesktop />
+    <App />
   </React.StrictMode>
 )
